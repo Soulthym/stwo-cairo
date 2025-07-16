5299d4ebe2aae9f8630146a59f81690dc98198eb
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-07-16 18:15:24 +0200

    test bincode

--- stwo_cairo_prover/crates/cairo-air/src/utils.rs
+++ stwo_cairo_prover/crates/cairo-air/src/utils.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
+use bincode;
 use serde::Serialize;
 use stwo::core::channel::MerkleChannel;
 use stwo::core::vcs::MerkleHasher;
@@ -19,8 +20,20 @@ pub enum ProofFormat {
     /// Compatible with `scarb execute`
     CairoSerde,
 }
+#[derive(Debug, Clone, Serialize)]
+struct EncodedFieldElement(starknet_ff::FieldElement);
+impl bincode::Encode for EncodedFieldElement {
+    fn encode<E: bincode::enc::Encoder>(
+        &self,
+        encoder: &mut E,
+    ) -> core::result::Result<(), bincode::error::EncodeError> {
+        bincode::Encode::encode(&self.0.to_bytes_be(), encoder)?;
+        Ok(())
+    }
+}
 
 /// Serializes Cairo proof given the desired format and writes it to a file.
+
 pub fn serialize_proof_to_file<MC: MerkleChannel>(
     proof: &CairoProof<MC::H>,
     proof_path: PathBuf,
@@ -41,13 +54,24 @@ where
         ProofFormat::CairoSerde => {
             let mut serialized: Vec<starknet_ff::FieldElement> = Vec::new();
             CairoSerialize::serialize(proof, &mut serialized);
-
-            let hex_strings: Vec<String> = serialized
-                .into_iter()
-                .map(|felt| format!("0x{:x}", felt))
-                .collect();
-
-            proof_file.write_all(sonic_rs::to_string_pretty(&hex_strings)?.as_bytes())?;
+            let serialized: Vec<EncodedFieldElement> =
+                serialized.into_iter().map(EncodedFieldElement).collect();
+            let Ok(bin_serialized) =
+                bincode::encode_to_vec(&serialized, bincode::config::standard())
+            else {
+                return Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    "Failed to serialize proof",
+                ));
+            };
+            println!("Serialized proof size: {}", bin_serialized.len());
+            proof_file.write_all(&bin_serialized)?;
+            // let hex_strings: Vec<String> = serialized
+            //     .into_iter()
+            //     .map(|felt| format!("0x{:x}", felt))
+            //     .collect();
+            //
+            // proof_file.write_all(sonic_rs::to_string_pretty(&hex_strings)?.as_bytes())?;
         }
     }
 
